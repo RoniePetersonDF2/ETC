72023298cd782526dd1cccd28dd56f64949207e3
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-02-28 16:57:48 -0300

    Adicionando exemplo aula 04

---
+++ LVII/aulas/04/operacao.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_GET['botaoEnviar'])) {
+    $operacao = $_GET['operacao'];
+    $valor1 = $_GET['valor1'];
+    $valor2 = $_GET['valor2'];
+    $resultado = 0;
+    if ($operacao == 'subtracao') {
+        $resultado = $valor1 - $valor2;
+    } elseif ($operacao == 'multiplicacao') {
+        $resultado = $valor1 * $valor2;
+    } elseif ($operacao == 'divisao') {
+        $resultado = $valor1 / $valor2;
+    } else {
+        $resultado = $valor1 + $valor2;
+    }
+}

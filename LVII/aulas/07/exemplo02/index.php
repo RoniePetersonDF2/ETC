b2f66e6857476b75269e42263888a6d30a55c389
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-03-13 14:17:54 -0300

    Add Aula 07 exemplo 02

--- LVII/aulas/07/exemplo02/index.php
+++ LVII/aulas/07/exemplo02/index.php
@@ -1,10 +1,20 @@
 <?php
     header('Content-Type: text/html; charset=utf-8');
+    
     // incluindo a classe aluno
     require_once "src/aluno.php";
-
     // instanciando objeto
     $a1 = new Aluno("123456-45", "Ronie Peterson");
+
+    // imprimindo objeto
+    echo '<pre>';
+    print_r($a1);
+
+    // exibindo o valor da matricula
+    echo $a1->matricula;
+    
+    //alterando o valor da matricula
+    $a1->matricula = "9.9999-99";
 
     // imprimindo objeto
     echo '<pre>';
---
+++ LVII/aulas/07/exemplo02/src/aluno.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Aluno {
+        private $matricula;
+        private $nome;
+
+        
+        public function __construct($matricula="", $nome="") {
+            $this->matricula = $matricula;
+            $this->nome = $nome;
+        }
+
+        public function __get($name) {
+            return $this->$name;
+        }
+
+        public function __set($name, $value) {
+            $this->$name = $value;
+        }
+
+    }

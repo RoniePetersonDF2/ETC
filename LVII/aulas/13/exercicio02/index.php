aed1d02b1ff8486dc4f70fa90deb7c3985219a5d
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-04-13 16:03:36 -0300

    Add aula 13 exercicios

--- LVII/aulas/13/exercicio02/index.php
+++ LVII/aulas/13/exercicio02/index.php
@@ -32,6 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 01</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
@@ -46,23 +47,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php } ?>
         <section>
             <form action="" method="post">
-                <label for="email" class="form-label">E-mail</label><br>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Informe seu e-mail (name@example.com)." required autofocus><br>
-                <label for="password" class="form-label">Password</label><br>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Informe seu password." required><br>
-                <label for="nome" class="form-label">Nome</label><br>
-                <input type="text" class="form-control" id="nome" name="nome" placeholder="Informe seu nome." required><br>
-                <label for="status" class="form-label">Status</label><br>
-                <select class="form-select" aria-label="Default select example" id="status" name="status">
-                    <option value="0">Inativo</option>
-                    <option value="1">Ativo</option>
-                </select>
-                <br><br>
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Informe seu e-mail (name@example.com)." required autofocus>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Informe seu password." required>
+                </div>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Informe seu nome." required>
+                </div>
+                <div class="mb-3">
+                    <label for="status" class="form-label">Status</label>
+                    <select class="form-select" aria-label="Default select example" id="status" name="status">
+                        <option value="0">Inativo</option>
+                        <option value="1">Ativo</option>
+                    </select>
+                </div>
 
                 <input type="submit" class="btn btn-primary" value="Salvar" id="btn-salvar" name="btn-salvar">
             </form>
         </section>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ LVII/aulas/13/exercicio02/scripts/script.sql
@@ -0,0 +1,14 @@
+--  apaga o banco de dados escolabd, se ele existir.
+DROP SCHEMA IF EXISTS escolabd;
+
+--  cria um banco de dados chamado escolabd.
+CREATE SCHEMA escolabd;
+
+--  cria uma tabela chamada usuarios com os campos: id, email, password, nome e status.
+CREATE TABLE escolabd.usuarios (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email       VARCHAR(255) NOT NULL,
+    password    VARCHAR(60) NOT NULL,
+    nome        VARCHAR(80) NOT NULL,
+    status      BOOLEAN NOT NULL  
+);

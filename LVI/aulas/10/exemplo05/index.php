ea0552cdefd527492c9d37734a8539580b580cd5
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-05-24 14:50:26 -0300

    Add aula 10 exemplos 5 e 6

---
+++ LVI/aulas/10/exemplo05/index.php
@@ -0,0 +1,18 @@
+<?php
+    $a = 10;
+    $b = 5;
+    
+    # verifica se o valor a é maior que b.
+    if(10 > 5)
+        echo "10 é maior 5. <br>";
+
+    # verifica se o valor a é menor que b.
+    if($b < $a) {
+        echo "$b é menor que $a. <br>";
+    }
+    
+    # verifica se o valor a é diferente b.
+    if($a != $b) {
+        echo "$a é diferente $b. <br>";
+    }
+    
---
+++ LVI/aulas/10/exemplo06/index.php
@@ -0,0 +1,18 @@
+<?php
+    $a = 10;
+    $b = 15;
+
+    # verifica se o valor a é maior que b.
+    if($a > $b)
+        echo "$a é maior $b. <br>";
+    else 
+        echo "$a não é maior que $b. <br>";
+
+    # verifica se o valor a é menor que b.
+    if($a < $b) {
+        echo "$a é menor que $b. <br>";
+    } else {
+        echo "$a é maior que $b. <br>";
+    }
+    
+    

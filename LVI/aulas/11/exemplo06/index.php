b9bc285df6fc54cd49cc6e5c5c9a21c6c046fde4
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-05-31 22:44:37 -0300

    Add aula 11 exemplos e exercicios

---
+++ LVI/aulas/11/exemplo06/index.php
@@ -0,0 +1,11 @@
+<?php
+    $marcas = array('GM', 'Honda', 'VW', 'Fiat');
+
+    var_dump($marcas);
+
+    $carros[] = 'Corsa';
+    $carros[] = 'Civic';
+    $carros[] = 'Polo';
+    $carros[] = 'Fiat 147';
+
+    var_dump($carros);
